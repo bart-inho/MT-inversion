e5a707f1baca1f49f1fd3ae3be46170591ad33d1
Brunodm52 <83716395+Brunodm52@users.noreply.github.com>
2021-05-06 15:09:12 +0200

    update MT_forward and Wait_recursion

--- MT code/MT_forward.m
+++ MT code/MT_forward.m
@@ -4,29 +4,30 @@ clear, close
 
 % Parameters
 load freq.mat;
-freq = freq';                       % [Hz] frequency of signal
-T = 1./freq;                        % [s] period of signal
-omega = 2*pi*freq;                 % [Hz] angular frequency of signal
-z = [0 200 400 1000]';%[0 4e3 4.5e3 5.5e3 8.5e3]';           % [m] depths
-                                    %     of ocean, sediments and two layers of oceanic crust
-sigma = [1e-3 1e-1 1e-2 1e-5]';%[3.2 1.5 0.03 0.004 1]';    % [S/m] electrical conductivites
-                                    %     of ocean, sediments and two layers of oceanic crust
+freq = freq'; % [Hz] frequency of signal
+T = 1./freq; % [s] period of signal
+omega = 2*pi*freq; % [Hz] angular frequency of signal
+z = [0 200 400 1000]'; % [m] depths
+sigma = [1e-3 1e-1 1e-2 1e-5]'; % [S/m] electrical conductivites
+fs = 13; % Fontsize
 
 % Variables
 mu_0 = 4*pi*1e-7; % [H/m] magnetic permeability of free space
 
-% Wait's recursion formula
-[C,rho_a,phi] = Wait_recursion(omega,z,sigma,mu_0);
+[C] = Wait_recursion(omega,z,sigma,mu_0); % C-response [1/m]
+rho_a = (real(C).^2+imag(C).^2)*mu_0.*omega; % Apparent resistivity [Ohm.m]
+phi = atand(imag(C)/real(C)) + 90; % Impedance phase [deg]
 
 % Regrouping data
 % data = [freq';rho_a';phi'];
 
-figure(),clf
+figure(1),clf
 loglog(T,rho_a,'-o')
 % plot(T, rho_a, '-o')
 grid on
-xlabel('Period T [s]')
-ylabel('Apparent resistivity \rho_a [\Omega\cdotm]')
+xlabel('Period T [s]','FontSize',fs)
+ylabel('Apparent resistivity \rho_a [\Omega\cdotm]','FontSize',fs)
+title('MT forward model with synthetic datas','FontSize',fs)
 
 
 
