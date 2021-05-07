62bc32d3cbe8a8e0b98389e8f929d7586af9ce1e
Bart Anhorn <barth.anhorn@icloud.com>
2021-05-07 16:39:12 +0200

    update dimension code

--- MT code/MT_dim.m
+++ MT code/MT_dim.m
@@ -1,17 +1,14 @@
 % Dimensionality
-
-% •	Z: les composants des tenseurs d'impédance 
-%   o	colonne 1 : Zxx
-%   o	colonne 2 : Zxy
-%   o	colonne 3 : Zyx
-%   o	colonne 4 : Zyy
-
-% dimension 1 : frequency
-% dimension 2 : impedence tensor
-% dimension 3 : datasts
+% -	Z: les composants des tenseurs d'impédance 
+%   -	colonne 1 : Zxx
+%   -	colonne 2 : Zxy
+%   -	colonne 3 : Zyx
+%   -	colonne 4 : Zyy
+%       - dimension 1 : Z relative to frequency
+%       - dimension 2 : impedence tensor
+%       - dimension 3 : datasets
 
 load Z
-load freq
 
 % equation (5.6)
 S1 = Z(:,1,:) + Z(:,4,:);
@@ -28,21 +25,15 @@ kappa = abs(S1)./abs(D2);
 % equation (5.21)
 mu = sqrt(commu(D1, S2) + commu(S1, D2))./abs(D2);
 
-class = zeros(size(Z,1), size(Z,3));
+class = zeros(size(Z,1), size(Z,3));        
 
-if kappa < 0.1
-    if sigma < 0.05
-        class(:,:) = 1; % class 1a (Simple 1-D model)
-    else
-        class(:,:) = 1.5; % class 1b (Simple 2-D model)
-    end
-else
-    if mu < 0.05
-        class(:,:) = 2; % class 2 (regional 1-D model with galvanic distortion)
-    else
-        class(:,:) = 3; % other shit
-    end
-end
+class(kappa< 0.1 & sigma< 0.05) = 1; % class 1a (Simple 1-D model)
+class(kappa< 0.1 & sigma>= 0.05) = 1.5; % class 1b (Simple 2-D model)
+class(kappa> 0.1 & mu < 0.05) = 2; % class 2 (regional 1-D model with galvanic distortion)
+class(kappa> 0.1 & mu>= 0.05) = 3; % other shit
+
+figure()
+heatmap(class)
 
 % (5.20)
 function commutator = commu(x, y)
