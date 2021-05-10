32fcce78d1a7fda7a033c2d75ebbe5fb20c585d8
Brunodm52 <83716395+Brunodm52@users.noreply.github.com>
2021-05-10 15:52:15 +0200

    update #1 de l'aprem

--- MT code/MT_inversion_1D.m
+++ MT code/MT_inversion_1D.m
@@ -24,21 +24,24 @@ Z(:,2,:) =  Z_B;
 Z(:,3,:) = -Z_B;
 
 % creation of the structure in depth
-nlayer = 5; % Number of layers
-thick = 50; % [m] Thickness of top layer
-sigma = 1e-1; % [S/m] electrical conductivity of top layer
-for j=2:nlayer
-    thick(end+1) = 3*thick(j-1); % [m] Thicknesses of layers
+nlayer = 21; % Number of layers
+thick = ones(nlayer,1);
+thick(1) = 50; % [m] Thickness of top layer
+sigma = ones(nlayer,1);
+sigma(1) = 1e-1; % [S/m] electrical conductivity of top layer
+for j=2:nlayer-1
+    thick(end+1) = 1.2*thick(j-1); % [m] Thicknesses of layers
     sigma(end+1) = 2*sigma(j-1); % [S/m] electrical conductivities of layers
-end
-
+end, clear j
+thick(end+1) = 60e3; % [m] Thickness of last layer
+sigma(end+1) = 2*sigma(end); % [S/m] electrical conductivity of last layer
 rho = 1./sigma; % [Ohm.m] resistivities of layers
-
 C = Wait_recursion(T,thick,rho); % C-response of data
 
-
-
 % starting model m_0
+dz = (0:50:2e4)';
+nz = length(dz);
+m0 = 0.02*ones(nz,1); % reference constant conductivity model (not considered if alphas=0)
 
 %% 1D inversion
 
@@ -51,16 +54,21 @@ C = Wait_recursion(T,thick,rho); % C-response of data
 % error matrix E
 
 % inversion
-    m_iter = m_start;
-    for iter = 1:25 % number of iterations (after 25 it doesnot change a 
-                    % lot anymore, but you can put more iterations if you 
-                    % want to check).
-        
-        dm = 1e-4; % delta_model for numerical differentiation
-        % [m_iter, chi2] = inversion_step(C, T, d_mod, m_iter, M, N, dm, E, lambda, D);
-        [m_iter, chi2] = inversion_step(C, T, thick_mod, m_iter, M, N, dm, E, lambda, D);
-        
-    end
-    m_end = m_iter;
-    
+m_iter = m_start;
+for iter = 1:25 % number of iterations (after 25 it doesnot change a 
+                % lot anymore, but you can put more iterations if you 
+                % want to check).
+
+    dm = 1e-4; % delta_model for numerical differentiation
+    % [m_iter, chi2] = inversion_step(C, T, d_mod, m_iter, M, N, dm, E, lambda, D);
+    [m_iter, chi2] = inversion_step(C, T, thick_mod, m_iter, M, N, dm, E, lambda, D);
+
+end
+m_end = m_iter;
+
 % plot of results
+
+
+
+
+
