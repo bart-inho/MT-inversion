e569dfc2c502b7c0d734d0df49fd15ca07999d1f
Brunodm52 <83716395+Brunodm52@users.noreply.github.com>
2021-05-11 12:20:52 +0200

    Update figures et position z (depths) dans code

--- MT code/MT_inversion_1D.m
+++ MT code/MT_inversion_1D.m
@@ -34,33 +34,32 @@ C_base = re_c + 1i*im_c;
 
 rho_a = abs(C_base).^2*4*pi*1e-7.*ome;
 
-% faire un joli plot
-figure(1), clf
-semilogy(log10(T), rho_a)
-
 phase = atand(im_c./re_c)+90;
-figure(2), clf
-plot(log10(1./freq), phase)
 
 % creation of the structure in depth
 nlayer = 21; % Number of layers
+
 thick = ones(nlayer,1);
-thick(1) = 50; % [m] Thickness of top layer
-sigma = ones(nlayer,1);
-% sigma(1) = 1e-1; % [S/m] electrical conductivity of top layer
+thick(1) = 50; % [m] Thickness of top layer = zeros(length(thick), 1);
 for j=2:nlayer-1
     thick(j) = 1.2*thick(j-1); % [m] Thicknesses of layers
-%     sigma(j) = 2*sigma(j-1); % [S/m] electrical conductivities of layers
 end, clear j
 thick(end) = 60e3; % [m] Thickness of last layer
+
+sigma = ones(nlayer,1); % [S/m] electrical conductivity of top layer
 sigma(:) = 1/rho_a(end); % [S/m] electrical conductivity of last layer
-% rho = 1./sigma; % [Ohm.m] resistivities of layers
-% C = Wait_recursion(T,thick,rho); % C-response of data
+
+z = zeros(size(thick));
+for i = 2:length(thick)
+%     z(i) = sum(thick(1:i));
+    z(i) = sum(z)+thick(i);
+end
+
 
 % starting model m_0
-dz = (0:50:2e4)';
-nz = length(dz);
-m0 = 0.02*ones(nz,1); % reference constant conductivity model (not considered if alphas=0)
+% dz = (0:50:2e4)';
+% nz = length(dz);
+% m0 = 0.02*ones(nz,1); % reference constant conductivity model (not considered if alphas=0)
 %% 1D inversion
 
 % Tip : you need to find the best lagrange lambda parameter that is in the
@@ -79,8 +78,10 @@ D(1, :) = 0;
 % Wm = D'*D;
 lamb_vec = logspace(-1, 4, 1e2)';
 
-chi2_tot = [];
-R1d = [];
+% chi2_tot = [];
+% R1d = [];
+chi2_tot = zeros(size(lamb_vec));
+R1d = chi2_tot;
 
 for s = 1:length(lamb_vec)
     lambda = lamb_vec(s);
@@ -111,18 +112,40 @@ for s = 1:length(lamb_vec)
     end
     m_end = m_iter;
     
-    chi2_tot = [chi2_tot;chi2];
-    R1d = [R1d;norm(D*m_end).^2];
+%     chi2_tot = [chi2_tot;chi2];
+%     R1d = [R1d;norm(D*m_end).^2];    
+    chi2_tot(s) = chi2;
+    R1d(s) = norm(D*m_end).^2;
 end
+
 
 % plot of results
-z = zeros(length(thick), 1);
-for i = 1:length(thick)
-    z(i) = sum(thick(1:i));
-end
+fs = 13; % ,'FontSize',fs
 
-figure(3), clf
+figure(1), clf
+sgtitle('Datas','FontSize',fs+2)
+xLim = [min(T) max(T)];
+subplot(2,1,1) % apparent resistivity
+loglog(T, rho_a)
+% xlabel('log_{10}T [s]')
+ylabel('Apparent resistivity \rho_a [\Omega\cdotm]','FontSize',fs)
+ylim([1 1e3])
+xlim(xLim)
+grid on
+subplot(2,1,2) % phase
+semilogx(T, phase)
+xlabel('Periods log_{10}T [s]','FontSize',fs)
+ylabel('Phase \phi [deg]','FontSize',fs)
+ylim([-180 180])
+xlim(xLim)
+grid on
+
+figure(2), clf
 plot(chi2_tot-M, R1d)
+title('L-curve','FontSize',fs)
+xlabel('\chi^{2}-M','FontSize',fs)
+ylabel('R1d','FontSize',fs)
+grid on
     
-% figure(4), clf
+% figure(3), clf
 % plot(m_end, z)
